8b812c856f43f06efb1434eff82e5b850fa606ce
Jasmin Paige <jaspagmaz98@gmail.com>
2023-04-28 15:40:20 +0800

    Python file with pseudocode

---
+++ multiline.py
@@ -0,0 +1,6 @@
+# Write a method in python to write multiple line of text contents into a text file mylife.txt.
+
+# Create a text file and name it mylife.txt
+    # Create a WHILE, and IF loop and ask the user to enter Lines.
+        # Ask the user if there are more lines they would like to add to the text file.
+        
